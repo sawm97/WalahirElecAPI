a106de6208702e92d2242922b6b46d4a1316c45f
sawm97 <sriariefwibawamukti@gmail.com>
2025-01-25 21:16:30 +0700

    edit format date middlewareLogRequest

--- middleware/logs.js
+++ middleware/logs.js
@@ -1,5 +1,19 @@
 const logRequest = (req, res, next) => {
-    console.log(`Request happens on:`, req.path, ` | Request type:`, req.method, ` | Request time:`, new Date());
+    let date = new Date();
+    date.setHours(date.getHours() + 7); // Menambahkan 7 jam
+
+    // Format tanggal ke format Indonesia (DD-MM-YYYY HH:mm:ss)
+    const formattedDate = date.toLocaleString('id-ID', {
+        day: '2-digit',
+        month: '2-digit',
+        year: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: false
+    });
+
+    console.log(`Request happens on:`, req.path, ` | Request type:`, req.method, ` | Request time:`, formattedDate);
     next();
 }
 
