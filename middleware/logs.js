84b9ccbaa5393ef8382bf6b5a41eab92823c39ea
sawm97 <sriariefwibawamukti@gmail.com>
2025-01-25 20:41:13 +0700

    edit middlewareLogRequest

--- middleware/logs.js
+++ middleware/logs.js
@@ -1,5 +1,7 @@
 const logRequest = (req, res, next) => {
     console.log(`Request happens on:`, req.path);
+    console.log(`Request type:`, req.method);
+    console.log(`Request time:`, new Date());
     next();
 }
 
