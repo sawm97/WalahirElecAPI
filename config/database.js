63467c34d76b42ed64aedd83a733185e656e4efb
Sri Arief Wibawa Mukti <sriariefwibawamukti@gmail.com>
2025-01-30 15:44:18 +0700

    fixing error port

--- config/database.js
+++ config/database.js
@@ -6,9 +6,10 @@ const config = {
     database: process.env.DB_NAME,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    port: process.env.PORT || 3000,
+    port: 1433,
     options: {
-        encrypt: true
+        encrypt: true,
+        enableArithAbort: true,
     }
 };
 
