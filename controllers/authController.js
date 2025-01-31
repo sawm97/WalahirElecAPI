7f907d90fb8ed679bbcb7ec64577e1f9264407b2
Sri Arief Wibawa Mukti <sriariefwibawamukti@gmail.com>
2025-01-31 10:45:42 +0700

    edit authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,15 +4,15 @@ const { createUser, getUserByEmail } = require('../models/userModel');
 require('dotenv').config();
 
 async function register(req, res) {
-    const { username, email, password } = req.body;
+    const { username, email, password, role} = req.body;
 
-    if (!username || !email || !password) {
+    if (!username || !email || !password || !role) {
         return res.status(400).json({ message: 'All fields are required' });
     }
 
     try {
         const hashedPassword = await bcrypt.hash(password, 10); // Hash password
-        await createUser(username, email, hashedPassword);
+        await createUser(username, email, hashedPassword, role);
         res.status(201).json({ message: 'User registered successfully' });
     } catch (err) {
         res.status(500).json({ message: err.message });
