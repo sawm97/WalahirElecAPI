8fef4747f1e9a577bbdb8c99ffc64526a3d59283
Sri Arief Wibawa Mukti <sriariefwibawamukti@gmail.com>
2025-02-10 16:20:19 +0700

    small edit route to get-profpic

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -21,6 +21,6 @@ router.put('/edit-user', AuthMiddleware.authenticateToken, UserController.update
 router.post('/edit-profpic', AuthMiddleware.authenticateToken, upload.single('profilePicture'), UserController.uploadProfilePicture);
 
 /* GET user profile picture */
-router.get('/profpic', AuthMiddleware.authenticateToken, UserController.viewImage);
+router.get('/get-profpic', AuthMiddleware.authenticateToken, UserController.viewImage);
 
 module.exports = router;
