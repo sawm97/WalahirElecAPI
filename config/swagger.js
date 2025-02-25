67a1561a6cd95dbfdc626079ba35d969f1752276
Sri Arief Wibawa Mukti <sriariefwibawamukti@gmail.com>
2025-02-25 16:09:06 +0700

    fixing swagger

--- config/swagger.js
+++ config/swagger.js
@@ -10,7 +10,7 @@ const options = {
         },
         servers: [
             {
-                url: 'https://walahirelecapi.azurewebsites.net/api-doc',
+                url: 'https://walahirelecapi.azurewebsites.net/api',
             },
         ],
     },
