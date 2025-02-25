b5cfbe1a78bf9ca7ec1f2b828d1580845370ae78
Sri Arief Wibawa Mukti <sriariefwibawamukti@gmail.com>
2025-02-25 15:58:47 +0700

    install swagger

---
+++ config/swagger.js
@@ -0,0 +1,22 @@
+const swaggerJSDoc = require('swagger-jsdoc');
+
+const options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'API Documentation',
+            version: '1.0.0',
+            description: 'API documentation for the backend',
+        },
+        servers: [
+            {
+                url: 'https://walahirelecapi.azurewebsites.net/api-doc',
+            },
+        ],
+    },
+    apis: ['../routes/*.js', '../models/*.js'], // Path to the API docs
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+module.exports = swaggerSpec;
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -3,22 +3,202 @@ const router = express.Router();
 const upload = require('../middleware/uploadMiddleware');
 const ProductController = require('../controllers/productController');
 
-// GET ALL PRODUCTS
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Product:
+ *       type: object
+ *       required:
+ *         - name
+ *         - stock
+ *         - price
+ *         - category_id
+ *       properties:
+ *         id:
+ *           type: integer
+ *           description: The auto-generated id of the product
+ *         name:
+ *           type: string
+ *           description: The name of the product
+ *         stock:
+ *           type: integer
+ *           description: The stock of the product
+ *         price:
+ *           type: integer
+ *           description: The price of the product
+ *         category_id:
+ *           type: integer
+ *           description: The category id of the product
+ *         description:
+ *           type: string
+ *           description: The description of the product
+ *       example:
+ *         id: 1
+ *         name: Laptop
+ *         stock: 10
+ *         price: 15000000
+ *         category_id: 1
+ *         description: High performance laptop
+ */
+
+/**
+ * @swagger
+ * tags:
+ *   name: Products
+ *   description: The products managing API
+ */
+
+/**
+ * @swagger
+ * /products:
+ *   get:
+ *     summary: Returns the list of all the products
+ *     tags: [Products]
+ *     responses:
+ *       200:
+ *         description: The list of the products
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Product'
+ */
 router.get('/', ProductController.fetchProducts);
 
-// GET PRODUCT BY ID
+/**
+ * @swagger
+ * /products/{id}:
+ *   get:
+ *     summary: Get the product by id
+ *     tags: [Products]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: integer
+ *         required: true
+ *         description: The product id
+ *     responses:
+ *       200:
+ *         description: The product description by id
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Product'
+ *       404:
+ *         description: The product was not found
+ */
 router.get('/:id', ProductController.getProduct);
 
-// CREATE PRODUCT
+/**
+ * @swagger
+ * /products:
+ *   post:
+ *     summary: Create a new product
+ *     tags: [Products]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Product'
+ *     responses:
+ *       201:
+ *         description: The product was successfully created
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Product'
+ *       500:
+ *         description: Some server error
+ */
 router.post('/', ProductController.addProduct);
 
-// UPDATE PRODUCT
+/**
+ * @swagger
+ * /products/{id}:
+ *   put:
+ *     summary: Update the product by the id
+ *     tags: [Products]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: integer
+ *         required: true
+ *         description: The product id
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Product'
+ *     responses:
+ *       200:
+ *         description: The product was updated
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Product'
+ *       404:
+ *         description: The product was not found
+ *       500:
+ *         description: Some error happened
+ */
 router.put('/:id', ProductController.editProduct);
 
-// DELETE PRODUCT
+/**
+ * @swagger
+ * /products/{id}:
+ *   delete:
+ *     summary: Remove the product by id
+ *     tags: [Products]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: integer
+ *         required: true
+ *         description: The product id
+ *     responses:
+ *       200:
+ *         description: The product was deleted
+ *       404:
+ *         description: The product was not found
+ */
 router.delete('/:id', ProductController.removeProduct);
 
-// UPLOAD PRODUCT IMAGE
+/**
+ * @swagger
+ * /products/{id}/upload-image:
+ *   post:
+ *     summary: Upload an image for the product
+ *     tags: [Products]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: integer
+ *         required: true
+ *         description: The product id
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         multipart/form-data:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               image:
+ *                 type: string
+ *                 format: binary
+ *     responses:
+ *       200:
+ *         description: The image was uploaded successfully
+ *       500:
+ *         description: Some error happened
+ */
 router.post('/:id/upload-image', upload.single('image'), ProductController.uploadProductImage);
 
 module.exports = router;
