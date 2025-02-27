a02b6801a77f960a61f98e1741b6a8a014242509
Sri Arief Wibawa Mukti <sriariefwibawamukti@gmail.com>
2025-02-27 13:33:27 +0700

    disabling ejs and change into swagger

--- app.js
+++ app.js
@@ -21,14 +21,14 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerSpec = require('./config/swagger');
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 app.use(middlewareLogRequest);
 
 app.use('/', indexRouter);
@@ -41,7 +41,7 @@ app.use('/api/categories', categoryRouter);
 app.use('/api/products', productRouter);
 
 // Swagger UI route
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
